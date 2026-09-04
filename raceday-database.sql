7f273ec170429023c6dd0fb9b2a41f4cef5f4bac
Nompumelelo Ngubane <u7672479295@gmail.com>
2026-09-04 22:29:52 +0200

    Update raceday-database.sql

--- raceday-database.sql
+++ raceday-database.sql
@@ -11,3 +11,21 @@ IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL DROP TABLE dbo.Events;
 IF OBJECT_ID('dbo.EmergencyContacts', 'U') IS NOT NULL DROP TABLE dbo.EmergencyContacts;
 IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL DROP TABLE dbo.Users;
 
+CREATE TABLE dbo.Users (
+    UserID INT IDENTITY(1,1) NOT NULL,
+    Email NVARCHAR (256) NOT NULL,
+    PasswordHash NVARCHAR(512) NOT NULL,
+    FirstName NVARCHAR(100) NOT NULL,
+    LastName NVARCHAR(100) NOT NULL,
+    Role NVARCHAR(50) NOT NULL,
+    ContactNumber NVARCHAR(20) NOT NULL,
+    ProfilePictureUrl NVARCHAR(1000) NOT NULL,
+    DateRegistered DATETIME2 NOT NULL,
+
+          CONSTRAINT DF_Users_DateRegistered DEFAULT GETDATE(),
+          CONSTRAINT PK_Users PRIMARY KEY CLUSTERED (UserID),
+          CONSTRAINT UQ_Users_Email UNIQUE NONCLUSTERED (Email),
+          CONSTRAINT CHK_User_Role_Check (Role IN ('Organiser', 'Participant')
+  );
+  
+    
