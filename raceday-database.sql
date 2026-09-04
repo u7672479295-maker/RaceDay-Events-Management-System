72c5bf7a97a8c4b052c7d11fc03574dbed126c0f
Nompumelelo Ngubane <u7672479295@gmail.com>
2026-09-04 22:15:16 +0200

    Update raceday-database.sql

--- raceday-database.sql
+++ raceday-database.sql
@@ -2,3 +2,12 @@
 -- RaceDay Event Management System
 
 --=====================================================================================
+
+
+IF OBJECT_ID('dbo.Results', 'U') IS NOT NULL DROP TABLE dbo.Results;
+IF OBJECT_ID('dbo.Enrolments', 'U') IS NOT NULL DROP TABLE dbo.Enrolments;
+IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL DROP TABLE dbo.Categories;
+IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL DROP TABLE dbo.Events;
+IF OBJECT_ID('dbo.EmergencyContacts', 'U') IS NOT NULL DROP TABLE dbo.EmergencyContacts;
+IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL DROP TABLE dbo.Users;
+
